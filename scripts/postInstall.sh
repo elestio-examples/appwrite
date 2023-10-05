af651cc7f73a46220f9e989deeb5a68909bd42fa
Amit Shukla <53058928+amitshuklabag@users.noreply.github.com>
2023-10-05 14:45:31 +0530

    Update postInstall.sh

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -5,9 +5,8 @@ set -o allexport; source .env; set +o allexport;
 echo "Waiting...";
 sleep 60s;
 
-target=$(docker-compose port traefik 80)
 
-  curl http://$target/v1/account \
+  curl https://${_APP_DOMAIN}/v1/account \
   -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.82 Safari/537.36' \
   -H 'content-type: application/json' \
   -H 'x-appwrite-locale: en' \
